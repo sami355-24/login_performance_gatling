badd6847a0b89877fa14e9310f22cca8aa912620
sami355-24 <ddung355@gmail.com>
2024-04-26 22:11:03 +0900

    누락된 테스트 코드들 추가

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -5,7 +5,23 @@ w
 Guser-files/simulations/computerdatabase/ComputerDatabaseSimulation.java,1/3/1365927322c55c91d2e3603503b5194a0f378114
 O
 user-files/resources/search.csv,d/1/d172506d15c2b08e0210416af4bac8b58958eac4
-o
-?results/computerdatabasesimulation-20240420020530905/index.html,0/e/0e257e4ac0f40c1ede27292e4cab66702b61446c
 [
 +gatling-charts-highcharts-bundle-3.10.5.iml,b/5/b5e8e465424dcc1aed4bee3412d9a037e06ae0a7
+?
+bin/gatling.bat,c/f/cf651c5546e0007bb8045fcf51605269dbda6a1d
+\
+,user-files/simulations/loginperformance.java,0/9/09b4f3c56d93c6e29d79eea8231427526e848580
+\
+,user-files/simulations/LoginPerformance.java,5/7/57d397c7a1de551a0de5268a4a33e02f5314989a
+X
+(user-files/simulations/ResponseBody.java,7/1/718e0237682ff60de6d5534c764e3bfc7aa7fe4d
+A
+conf/gatling.conf,b/6/b69c6e14f242ffcb93c726f08cc5e93c6555276b
+F
+conf/gatling-akka.conf,3/b/3b49bdbc7b13ed6c03e687cf95dd150c38b5b676
+@
+conf/logback.xml,f/0/f07f9011ad49a459acd231b29e73f4089088bae4
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+;
+gatling.log,7/9/796f104e1117e6ad56e8221593d17aa133e11b85
--- user-files/simulations/LoginPerformance.java
+++ user-files/simulations/LoginPerformance.java
@@ -1,2 +1,102 @@
-package PACKAGE_NAME;public class loginperformance {
+import static io.gatling.javaapi.core.CoreDsl.StringBody;
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.feed;
+import static io.gatling.javaapi.core.CoreDsl.incrementUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+import io.gatling.javaapi.http.HttpRequestActionBuilder;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class LoginPerformance extends Simulation {
+
+    private final HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8081")
+            .contentTypeHeader("application/json");
+
+    private static final Random random = new Random();
+
+    private static final Iterator<Map<String, Object>> randomFeeder = Stream.generate(
+            (Supplier<Map<String, Object>>) () -> Map.of("randomNumber", random.nextInt(50) + 1)).iterator();
+
+
+    private final ChainBuilder loginScenario =
+            feed(randomFeeder)
+                    .exec(login("1-a. 로그인"));
+
+    private final ChainBuilder simpleRequestScenario =
+            feed(randomFeeder)
+                    .exec(login("1-a. 로그인"))
+                    .exec(fakeBusinessLogic("1-b. 비즈니스 로직"));
+
+    private final ChainBuilder loadRequestScenario =
+            feed(randomFeeder)
+                    .exec(login("1-a. 로그인"))
+                    .exec(fakeBusinessLogic("1-b. 비즈니스 로직"))
+                    .exec(realBusinessLogic("1-c. 비즈니스 로직"));
+
+
+
+    private static HttpRequestActionBuilder login(String name) {
+        return http(name)
+                .post("/auth/login")
+                .body(StringBody("""
+                                                  {
+                                                  "email": "user#{randomNumber}@test.com",
+                                                  "password": "1234"
+                                                  }
+                        """))
+                .check(status().is(200));
+    }
+
+    private static HttpRequestActionBuilder fakeBusinessLogic(String name) {
+        return http(name)
+                .get("/load/fake")
+                .check(status().is(200));
+    }
+
+    private static HttpRequestActionBuilder realBusinessLogic(String name) {
+        return http(name)
+                .get("/load/real")
+                .check(status().is(200));
+    }
+
+    private final ScenarioBuilder scenario1 = scenario("1번 시나리오 유저").exec(loginScenario);
+    private final ScenarioBuilder scenario2 = scenario("2번 시나리오 유저").exec(simpleRequestScenario);
+    private final ScenarioBuilder scenario3 = scenario("3번 시나리오 유저").exec(loadRequestScenario);
+    {
+        setUp(
+//                scenario1.injectOpen(
+//                        incrementUsersPerSec(1)
+//                                .times(30) // 테스트가 끝날 때까지 사용자 수를 지속적으로 증가시킴
+//                                .eachLevelLasting(1) // 각 사용자 수를 1초 동안 유지
+//                                .startingFrom(1) // 사용자 수를 1명부터 시작
+//                ),
+//
+//                scenario2.injectOpen(
+//                        incrementUsersPerSec(1)
+//                                .times(30) // 테스트가 끝날 때까지 사용자 수를 지속적으로 증가시킴
+//                                .eachLevelLasting(1) // 각 사용자 수를 1초 동안 유지
+//                                .startingFrom(1) // 사용자 수를 1명부터 시작
+//                ),
+
+                scenario3.injectOpen(
+                        incrementUsersPerSec(1)
+                                .times(10) // 테스트가 끝날 때까지 사용자 수를 지속적으로 증가시킴
+                                .eachLevelLasting(1) // 각 사용자 수를 1초 동안 유지
+                                .startingFrom(1) // 사용자 수를 1명부터 시작
+                )
+        ).protocols(httpProtocol);
+    }
+
+
 }
